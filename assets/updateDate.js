f1dc1c8bd9bef031624f6c8c93d69f03cdd5168e
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2024-11-25 22:07:43 +0545

    Fix time update

--- assets/updateDate.js
+++ assets/updateDate.js
@@ -1,8 +1,8 @@
 function updateDate() {
-  const currentDate = new Date();
-  let day = currentDate.getDate();
-  let month = currentDate.getMonth() + 1;
-  const year = currentDate.getFullYear();
+  const newDate = new Date();
+  let day = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  const year = newDate.getFullYear();
   day = day < 10 ? `0${day}` : day;
   month = month < 10 ? `0${month}` : month;
   const dateSection = document.querySelector(".date-section");
