c34850301831a141547c85e0df622527128a5031
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2025-01-29 15:48:39 +0545

    Logic for Component Switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,59 @@
-import React, { useState } from "react";
 import "./App.css";
+import React, { useState } from "react";
 import { Button } from "./components/ui/button";
 import { Home } from "lucide-react";
 import HomePage from "./components/HomePage";
+import ProfilePage from "./components/ProfilePage";
+import ProjectPage from "./components/ProjectPage";
 
 function App() {
+  const [isHome, setIsHome] = useState(true);
+  const [isProfile, setIsProfile] = useState(false);
+  const [isProject, setIsProject] = useState(false);
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-800 via-slate-800 to-gray-900 grid grid-rows-[0.1fr_1fr]">
       <div className="flex justify-center items-center gap-10 p-10">
-        <Button className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 hover:from-gray-200 hover:via-gray-300 hover:to-gray-400 text-gray-800 transition duration-300 hover:shadow-md hover:shadow-white text-lg hover:scale-105 active:scale-95">
+        <Button
+          onClick={() => {
+            setIsHome(false);
+            setIsProfile(true);
+            setIsProject(false);
+          }}
+          className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 hover:from-gray-200 hover:via-gray-300 hover:to-gray-400 text-gray-800 transition duration-300 hover:shadow-md hover:shadow-white text-lg hover:scale-105 active:scale-95"
+        >
           Profile
         </Button>
-        <Button className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 hover:from-gray-200 hover:via-gray-300 hover:to-gray-400 text-gray-800 transition duration-300 hover:shadow-md hover:shadow-white text-lg hover:scale-105 active:scale-95 rounded-full w-12 h-12">
+        <Button
+          onClick={() => {
+            setIsHome(true);
+            setIsProfile(false);
+            setIsProject(false);
+          }}
+          className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 hover:from-gray-200 hover:via-gray-300 hover:to-gray-400 text-gray-800 transition duration-300 hover:shadow-md hover:shadow-white text-lg hover:scale-105 active:scale-95 rounded-full w-12 h-12"
+        >
           <Home className="scale-150" />
         </Button>
-        <Button className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 hover:from-gray-200 hover:via-gray-300 hover:to-gray-400 text-gray-800 transition duration-300 hover:shadow-md hover:shadow-white text-lg hover:scale-105 active:scale-95">
+        <Button
+          onClick={() => {
+            setIsHome(false);
+            setIsProfile(false);
+            setIsProject(true);
+          }}
+          className="bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300 hover:from-gray-200 hover:via-gray-300 hover:to-gray-400 text-gray-800 transition duration-300 hover:shadow-md hover:shadow-white text-lg hover:scale-105 active:scale-95"
+        >
           Project
         </Button>
       </div>
 
-      <div className="self-end">
-        <HomePage />
-      </div>
+      {isHome ? (
+        <div className="self-end">
+          <HomePage />
+        </div>
+      ) : (
+        <div className="self-center text-white">
+          {isProfile ? <ProfilePage /> : <ProjectPage />}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/ProjectPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <div className="text-white">
+        project
+    </div>
+  )
+}
+
+export default ProjectPage
