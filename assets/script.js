41db82166296db3a2226b49535d496945edb7a1e
Aaryan Dhakal <aaryandhakal2020@gmail.com>
2025-01-29 13:53:08 +0545

    Switch to React

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-import { updateTime } from "./updateTime.js";
-updateTime();
-setInterval(updateTime, 1000);
-
-import { updateDate } from "./updateDate.js";
-updateDate();
-
-import { updateNavig } from "./updateNavig.js";
-updateNavig();
